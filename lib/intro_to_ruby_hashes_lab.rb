891c356fc1fbc295f526295cc94fcb6d05bb0658
Steven Lee <steven.lee1393@gmail.com>
2020-06-30 19:45:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def new_hash
   # return an empty hash
+  {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  myhash = {
+    :name => "Steven"
+  }
 end
 
 def pioneer
